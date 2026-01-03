d6c81faf6aa585117c91aebdb245b9af8deddaed
raziman <raziman@tutanota.com>
2026-01-03 11:32:58 +0800

    update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    const baseUrl = "https://maybankconverter.vercel.app";
+    const baseUrl = "https://maybankconverter.raziman.online";
     const currentDate = new Date();
 
     return [
